df4e37e0f43131d1b8f859a9b79d4e02dc96dd79
MUTHU KISHORE <muthukishorep.com>
2026-02-05 07:51:48 +0530

    H/W

---
+++ conditions(day 6)/Voting.java
@@ -0,0 +1,14 @@
+class Voting
+ {
+    public static void main(String[] args) 
+	{
+        
+        int age = 21; 
+
+        if (age >= 18) 
+            System.out.println("Eligible to vote");
+        else 
+            System.out.println("Not eligible to vote");
+        
+    }
+}
---
+++ conditions(day 6)/largestNumbers.java
@@ -0,0 +1,15 @@
+class largestNumbers
+ {
+    public static void main(String[] args) 
+	{
+        
+        int num1 = 18; 
+        int num2 = 20; 
+
+        if (num1<num2) 
+            System.out.println(num1 + " is the largest number");
+         else if (num2>num1)
+            System.out.println(num2 + " is the largest number");
+        
+    }
+}
