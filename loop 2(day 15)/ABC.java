c709508605b4628b31d13819edc91bee1c838f9a
MUTHU KISHORE <muthukishorep.com>
2026-02-12 22:23:18 +0530

    H/W

---
+++ loop 2(day 15)/ABC.java
@@ -0,0 +1,17 @@
+class ABC
+{
+	public static void main(String[] args)
+	{
+		int j = 5;
+		
+		for(int k = 1; k<= j; k++)
+		{
+			 for (int d = 1; d <= k; d++)
+			 {
+				System.out.print(d +""); 
+			 }
+			 System.out.println();
+		}
+		
+	}
+}
